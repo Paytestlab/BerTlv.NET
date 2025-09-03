37850d6e5862fa8195b8575b8196eee759ac2864
Matija Mazalin <matija.mazalin@paytestlab.com>
2025-09-03 08:28:28 +0200

    Migrate to .net8, updated tests.

--- Test/TlvTest.cs
+++ Test/TlvTest.cs
@@ -9,24 +9,24 @@ namespace Test
     public class TlvTest
     {
         // Verify: http://www.emvlab.org/tlvutils/?data=6F1A840E315041592E5359532E4444463031A5088801025F2D02656E
-        private string _validAsciiHexString = "6F1A840E315041592E5359532E4444463031A5088801025F2D02656E";
+        private readonly string _validAsciiHexString = "6F1A840E315041592E5359532E4444463031A5088801025F2D02656E";
 
         // Note: emvlab does not correctly parse inline and trailing padding (0x00, 0xFF), but the specification 
         // allows for it. The TLV string below should be parsed in the same way as the one above
-        private string _validAsciiHexStringWithPadding = "006F1B840E315041592E5359532E444446303100A5088801025F2D02656E0000";
+        private readonly string _validAsciiHexStringWithPadding = "006F1B840E315041592E5359532E444446303100A5088801025F2D02656E0000";
 
         [Fact]
         [Trait("Build", "Run")]
         public void ShouldThrowExceptionOnEmptyString()
         {
-            Assert.Throws<ArgumentException>(() => { var tlvs = Tlv.ParseTlv(""); });
+            Assert.Throws<ArgumentException>(() => { Tlv.ParseTlv(""); });
         }
 
         [Fact]
         [Trait("Build", "Run")]
         public void ShouldThrowExceptionOnEmptyHexArray()
         {
-            Assert.Throws<ArgumentException>(() => { var tlvs = Tlv.ParseTlv(new byte[] { }); });
+            Assert.Throws<ArgumentException>(() => {  Tlv.ParseTlv(new byte[] { }); });
         }
 
         [Fact]
@@ -77,31 +77,26 @@ namespace Test
             AssertTlv(tlvs);
         }
 
-        private void AssertTlv(ICollection<Tlv> tlvs)
+        private static void AssertTlv(ICollection<Tlv> tlvs)
         {
             Assert.NotNull(tlvs);
-            Assert.True(tlvs.Count == 1);
+            Assert.Single(tlvs);
 
-            var _6F = tlvs.SingleOrDefault(t => t.HexTag == "6F");
-            Assert.NotNull(_6F);
-            Assert.True(_6F.Children.Count == 2);
+            var _6F = tlvs.Single(t => t.HexTag == "6F");
+            Assert.Equal(2, _6F.Children.Count);
 
-            var _6F84 = _6F.Children.SingleOrDefault(t => t.HexTag == "84");
-            Assert.NotNull(_6F84);
-            Assert.True(_6F84.Children.Count == 0);
-            Assert.True(_6F84.HexValue == "315041592E5359532E4444463031");
+            var _6F84 = _6F.Children.Single(t => t.HexTag == "84");
+            Assert.Empty(_6F84.Children);
+            Assert.Equal("315041592E5359532E4444463031", _6F84.HexValue);
 
-            var _6FA5 = _6F.Children.SingleOrDefault(t => t.HexTag == "A5");
-            Assert.NotNull(_6FA5);
-            Assert.True(_6FA5.Children.Count == 2);
+            var _6FA5 = _6F.Children.Single(t => t.HexTag == "A5");
+            Assert.Equal(2, _6FA5.Children.Count);
 
-            var _6FA588 = _6FA5.Children.SingleOrDefault(t => t.HexTag == "88");
-            Assert.NotNull(_6FA588);
-            Assert.True(_6FA588.HexValue == "02");
+            var _6FA588 = _6FA5.Children.Single(t => t.HexTag == "88");
+            Assert.Equal("02", _6FA588.HexValue);
 
-            var _6FA55F2D = _6FA5.Children.SingleOrDefault(t => t.HexTag == "5F2D");
-            Assert.NotNull(_6FA55F2D);
-            Assert.True(_6FA55F2D.HexValue == "656E");
+            var _6FA55F2D = _6FA5.Children.Single(t => t.HexTag == "5F2D");
+            Assert.Equal("656E", _6FA55F2D.HexValue);
         }
 
     }
